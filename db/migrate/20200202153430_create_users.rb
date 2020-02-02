0221ee34ebfe758fd535ce90b192fe45e52d0406
leva84 <denfrut8@gmail.com>
2020-02-02 18:39:39 +0300

    Model and controller users

---
+++ db/migrate/20200202153430_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :phone
+      t.string :password
+      t.string :avatar_url
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_02_114458) do
+ActiveRecord::Schema.define(version: 2020_02_02_153430) do
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2020_02_02_114458) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "phone"
+    t.string "password"
+    t.string "avatar_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
